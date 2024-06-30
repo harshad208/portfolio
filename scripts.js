3850b5b13bba3a78e9a7101a84535bf556645398
Harshad Yadav <harshadyadav20@gmail.com>
2024-06-30 22:52:01 +0530

    Major changes

--- scripts.js
+++ scripts.js
@@ -4,3 +4,22 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Portfolio Loaded');
 });
+
+
+document.getElementById('sidebar-toggle').addEventListener('click', function() {
+    document.getElementById('sidebar').classList.toggle('show');
+});
+
+function openModal(modalId) {
+    var modal = document.getElementById(modalId);
+    if (modal) {
+        modal.style.display = "block";
+    }
+}
+
+function closeModal(modalId) {
+    var modal = document.getElementById(modalId);
+    if (modal) {
+        modal.style.display = "none";
+    }
+}
